e68f6c1a9dd1cd28032b8aa0cbec50e0f2fb9e96
Gemma Beferull Calabuig <gemmabecala@gmail.com>
2018-09-17 18:57:38 +0200

    footer and responsive styles

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import PhoneListContainer from "./components/PhoneListContainer/PhoneListContainer";
 import Header from "./components/Header/header";
+import Footer from "./components/Footer/Footer";
 
 class App extends Component {
   render() {
@@ -9,6 +10,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <PhoneListContainer />
+        <Footer />
       </div>
     );
   }
---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./footer.css";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <a href="https://github.com/GemmaBeferull/catalog">
+          Github GemmaBeferull
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Footer;
